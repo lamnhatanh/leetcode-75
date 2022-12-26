0f13d39d2830dae1e21e9d0d9ad51bd2d0648f06
Anh Lam <48115741+lamnhatanh@users.noreply.github.com>
2022-12-25 22:39:13 -0800

    Create CanPlaceFlowers.java

---
+++ src/Array/CanPlaceFlowers.java
@@ -0,0 +1,32 @@
+public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int placeFlower = 0;
+        
+        if (flowerbed.length == 1 && flowerbed[0] == 0) {
+            return true;
+        }
+
+        for (int i = 0; i < flowerbed.length; i++) {
+            if (flowerbed[i] == 0) {
+                if (i == 0) {
+                    if (flowerbed[i+1] == 0) {
+                        placeFlower++;
+                        flowerbed[i] = 1;
+                    }
+                    
+                } else if (i == flowerbed.length - 1) {
+                    if (flowerbed[i-1] == 0) {
+                        placeFlower++;
+                        flowerbed[i] = 1;
+                    }
+                    
+                } else {
+                    if (flowerbed[i-1] == 0 && flowerbed[i+1] == 0) {
+                        placeFlower++;
+                        flowerbed[i] = 1;
+                    }
+                }
+            }
+        }
+
+        return placeFlower >= n;
+    }
